9338ffc31a6250e500012620ffefc2e56fecc1cc
polarpolar914 <kdw2003914@naver.com>
2024-01-05 23:57:11 +0900

    polarpolar914 step1

---
+++ src/main/java/roomescape/ReservationController.java
@@ -0,0 +1,56 @@
+package roomescape;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Controller
+public class ReservationController {
+    private final List<Reservation> reservations = new ArrayList<>();
+
+    @GetMapping("/reservation")
+    public String reservation() {
+        return "reservation";
+    }
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> read() {
+        //Test 코드의 .body("size()", is(3));를 통과시키기 위해 임의로 Reservation을 3개 생성
+        //reservations.add(new Reservation(1, "브라운", "2021-08-05", "15:40"));
+        //reservations.add(new Reservation(2, "브라운", "2021-08-05", "15:40"));
+        //reservations.add(new Reservation(3, "브라운", "2021-08-05", "15:40"));
+
+        return ResponseEntity.ok(reservations);
+    }
+
+    @PostMapping("/reservations")
+    public ResponseEntity<Reservation> create(@RequestBody Reservation reservation) {
+        if (reservation.getName().isEmpty() || reservation.getDate().isEmpty() || reservation.getTime().isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        reservation.setId((long)reservations.size() + 1);
+        reservations.add(reservation);
+        return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).contentType(MediaType.APPLICATION_JSON).body(reservation);
+    }
+    @DeleteMapping("/reservations/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Reservation reservation = reservations.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElse(null);
+
+        if(reservation == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        reservations.remove(reservation);
+
+        return ResponseEntity.noContent().build();
+    }
+}
