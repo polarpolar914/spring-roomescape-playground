a86ff9dfabb001bbc93adc24c616866f508f1faa
polarpolar914 <kdw2003914@naver.com>
2024-01-07 20:30:54 +0900

    ADD mapping method to hello.html for index page

---
+++ src/main/java/roomescape/RoomescapeController.java
@@ -0,0 +1,11 @@
+package roomescape;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class RoomescapeController {
+    // index 페이지를 /resources/templates/hello.html로 설정
+    @GetMapping("/")
+    public String index() {
+        return "hello";
+    }
+}
