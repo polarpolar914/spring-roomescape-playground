e7bbc76dfc7fb7b45d58e44a7fd19885ce38dec8
polarpolar914 <kdw2003914@naver.com>
2024-01-14 16:05:00 +0900

    feat(단계6): ReservationQueryingDAO에 reservationRowMapper추가

--- src/main/java/roomescape/ReservationQueryingDAO.java
+++ src/main/java/roomescape/ReservationQueryingDAO.java
@@ -1,12 +1,16 @@
 package roomescape;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReservationQueryingDAO {
     private final JdbcTemplate jdbcTemplate;
-
+    private final RowMapper<Reservation> reservationRowMapper = (resultSet, rowNum) -> {
+        Reservation reservation = new Reservation(resultSet.getLong("id"), resultSet.getString("name"), resultSet.getString("date"), resultSet.getString("time"));
+        return reservation;
+    };
 
     public ReservationQueryingDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
