3ca4ed62f47236b5914ddf7d1cf90042add888d7
polarpolar914 <kdw2003914@naver.com>
2024-01-19 00:34:20 +0900

    feature: ReservationUpdatingDAO의 delete에서 NotFoundReservationException을 명시적으로 사용

--- src/main/java/roomescape/ReservationUpdatingDAO.java
+++ src/main/java/roomescape/ReservationUpdatingDAO.java
@@ -1,11 +1,13 @@
 package roomescape;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Repository
 public class ReservationUpdatingDAO {
@@ -48,9 +50,15 @@ public class ReservationUpdatingDAO {
     public boolean delete(Long id) {
         String sql = "DELETE FROM reservation WHERE id = ?";
         int num = jdbcTemplate.update(sql, id);
-        if (num == 0) {
+        if (num == 0) { // 삭제할 대상이 없는 경우
+            handleNotFoundReservationException(new NotFoundReservationException("Reservation is not found in DB with id: " + id));
             return false;
         }
         return true;
+    }
+
+    @ExceptionHandler(NotFoundReservationException.class)
+    public ResponseEntity handleNotFoundReservationException(NotFoundReservationException e) {
+        return ResponseEntity.badRequest().build();
     }
 }
