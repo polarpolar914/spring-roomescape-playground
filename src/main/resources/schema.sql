4c4460ca32ef54be09ba5e729bf2728a8c86a165
polarpolar914 <kdw2003914@naver.com>
2024-01-21 18:50:52 +0900

    feat(단계8): schema.sql에 time 테이블 DDL 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,14 @@
 CREATE TABLE reservation
 (
-    id      BIGINT       NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(255) NOT NULL,
-    date    VARCHAR(255) NOT NULL,
-    time    VARCHAR(255) NOT NULL,
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date VARCHAR(255) NOT NULL,
+    time VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE time
+(
+    id   BIGINT       NOT NULL AUTO_INCREMENT,
+    time VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
