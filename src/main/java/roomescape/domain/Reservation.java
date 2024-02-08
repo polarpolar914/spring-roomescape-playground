03e32aa20b058ce454edc7adf0dff1731ba2092d
polarpolar914 <kdw2003914@naver.com>
2024-02-08 09:08:46 +0900

    refactor: 더이상 사용하지 않는 setId를 모든 도메인에서 제거

--- src/main/java/roomescape/domain/Reservation.java
+++ src/main/java/roomescape/domain/Reservation.java
@@ -24,10 +24,6 @@ public class Reservation {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
--- src/main/java/roomescape/domain/Time.java
+++ src/main/java/roomescape/domain/Time.java
@@ -24,8 +24,4 @@ public class Time {
     public long getId() {
         return this.id;
     }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 }
